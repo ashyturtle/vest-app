86931b24146ce8a475c5b6adb94156b92382cb0a
ashyj <ashyjacklin@gmail.com>
2024-08-06 16:22:08 -0700

    user page init

--- lib/musicPlayerPage.dart
+++ lib/musicPlayerPage.dart
@@ -7,43 +7,37 @@ class MusicPlayerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:'Flutter Just Audio',
+      title: 'Flutter Just Audio',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch:Colors.blue),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          leading: IconButton(
-            onPressed: (){},
-            icon: const Icon(Icons.keyboard_arrow_down_rounded),
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            leading: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.keyboard_arrow_down_rounded),
+            ),
+            actions: [
+              IconButton(onPressed: () {}, icon: const Icon(Icons.more_horiz))
+            ],
           ),
-          actions:[
-            IconButton(
-            onPressed: (){},
-            icon: const Icon(Icons.more_horiz)
-            )
-          ],
-        ),
-         body:Container(
-         padding: const EdgeInsets.all(20),
-         height: double. infinity,
-         width: double. infinity,
-         decoration: const BoxDecoration(
-         gradient: LinearGradient(
-           begin: Alignment. topCenter,
-           end: Alignment. bottomCenter,
-           colors: [ Color(0xFF144771), Color(0xFF071A2C)],
-           
-         ) ,
-         ) ,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment. center,
-        children: const [],
-      ),
-      )
-      ),
-
+          body: Container(
+            padding: const EdgeInsets.all(20),
+            height: double.infinity,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color(0xFF144771), Color(0xFF071A2C)],
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [],
+            ),
+          )),
     );
   }
 }
--- lib/userPage.dart
+++ lib/userPage.dart
@@ -12,17 +12,51 @@ class _UserPageState extends State<UserPage> {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      scrollDirection: Axis.vertical,
-      padding: EdgeInsets.all(8),
-      children: [
-        Container(
-          height: 150,
-          child: Card(
-            color: MyApp.accentColor,
-            child: Text("Something"),
+        scrollDirection: Axis.vertical,
+        padding: EdgeInsets.all(8),
+        children: [
+          Container(
+            height: 150,
+            child: Card(
+              color: MyApp.accentColor,
+              child: Center(
+                child: Text(
+                  "My Account",
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              //Icon()
+            ),
           ),
-        )
-      ]
-    );
+          Container(
+            height: 150,
+            child: Card(
+              color: MyApp.accentColor,
+              child: Center(child: Text("My Account")),
+            ),
+          ),
+          Container(
+            height: 150,
+            child: Card(
+              color: MyApp.accentColor,
+              child: Center(
+                child: Text("Something"),
+              ),
+            ),
+          ),
+          Container(
+            height: 150,
+            child: Card(
+              color: MyApp.accentColor,
+              child: Center(
+                child: Text("Settings"),
+              ),
+            ),
+          ),
+        ]);
   }
 }
