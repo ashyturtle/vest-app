16582e1b9884e0362ca9780e2f97db66f378979b
ashyj <ashyjacklin@gmail.com>
2024-08-31 13:57:15 -0700

    User page updated

--- lib/userPage.dart
+++ lib/userPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vest1/accountInformation.dart';
 import 'package:vest1/helpPage.dart';
 import 'package:vest1/main.dart';
 import 'package:vest1/myAccount.dart';
@@ -11,69 +12,137 @@ class UserPage extends StatefulWidget {
 }
 
 class _UserPageState extends State<UserPage> {
+  String name = "Jack";
+  String account = "ashyjacklin@gmail.com";
+  String firstInitial = 'J';
   @override
   Widget build(BuildContext context) {
     return ListView(
         scrollDirection: Axis.vertical,
         padding: EdgeInsets.all(8),
         children: [
-
-          Container(
-            height: 75,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(width: 8),
-                Icon(Icons.fitness_center_rounded, size: 24),
-                SizedBox(width: 8,),
-                Text("Fitness",
+          Center(
+            child: Container(
+              height: 100,
+              width: 100,
+              //decoration: BoxDecoration(
+              //shape: BoxShape.circle),
+              child: Card(
+                  elevation: 10,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                  margin: EdgeInsets.all(0),
+                  child: Center(
+                    child: Text(firstInitial,
+                        style: TextStyle(
+                          fontSize: 50,
+                        )),
+                  )),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Container(
+                child: Text(account,
                     style: TextStyle(
-                        fontSize: 24)),
-              ],
-            ),
+                      fontSize: 20,
+                      fontWeight: FontWeight.w100,
+                    ))),
+          ]),
+          SizedBox(
+            height: 5,
           ),
           Container(
-            height: 75,
-            child: InkWell(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => HelpPage()));
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(width: 8),
-                  Icon(Icons.lightbulb_outline, size: 24),
-                  SizedBox(width: 8,),
-                  Text("Help",
-                      style: TextStyle(
-                          fontSize: 24)),
-                ],
-              ),
-            ),
+            height: 2,
+            color: MyApp.surfaceColor,
           ),
-          Container(
-            height: 75,
-            child: InkWell(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => HelpPage()));
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(width: 8),
-                  Icon(Icons.settings, size: 24),
-                  SizedBox(width: 8),
-                  Text("Settings",
-                      style: TextStyle(
-                          fontSize: 24)),
-                ],
-              ),
-            ),
+          SizedBox(
+            height: 20,
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => AccountInformation()));
+            },
+            child: Container(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.info_outline_rounded),
+                          SizedBox(width: 8,),
+                          Text(
+                            "Account information",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w200,
+                              fontSize: 25,
+                            ),
+                          ),
+                        ],
+                      ),
+                  Icon(Icons.chevron_right)
+                ])),
+          ),
+          SizedBox(height:16),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => HelpPage()));
+            },
+            child: Container(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.lightbulb_outline),
+                          SizedBox(width: 8,),
+                          Text(
+                            "Help",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w200,
+                              fontSize: 25,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Icon(Icons.chevron_right)
+                    ])),
+          ),
+          SizedBox(height:16),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => AccountInformation()));
+            },
+            child: Container(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.settings_outlined),
+                          SizedBox(width: 8,),
+                          Text(
+                            "Settings",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w200,
+                              fontSize: 25,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Icon(Icons.chevron_right)
+                    ])),
           ),
         ]);
   }
