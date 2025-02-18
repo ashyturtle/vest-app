d6a2e1693523acb7cabe7c195581a4a8cd4e5693
Soroush Mirzaee <72804931+DeltaTheM2@users.noreply.github.com>
2025-02-17 23:59:43 -0800

    Revert "mapbox added"
    This reverts commit eff294bb6174d8c9ecaaec765d08405ec777774f.

--- android/app/src/main/kotlin/com/codingmind/vest/MainActivity.kt
+++ android/app/src/main/kotlin/com/codingmind/vest/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.codingmind.pulsepath
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity(){
-
-}
+class MainActivity: FlutterActivity()
